b833be298e7280d1ad6740ec197104a18b469d83
badou11 <mcabpn@gmail.com>
2020-08-05 16:17:57 +0000

    addidng header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { Route } from '@angular/compiler/src/core';
+import { Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes:Routes=[
+  {path: 'header', component:HeaderComponent},
+  {path: 'footer', component:FooterComponent},
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
