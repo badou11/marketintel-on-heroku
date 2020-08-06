549bfebe8505cfde60d358e03f525b18563ef3a6
badou11 <mcabpn@gmail.com>
2020-08-06 12:25:01 +0000

    Adding somes changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { Route } from '@angular/compiler/src/core';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { EcoleComponent } from './layout/ecole/ecole.component';
+import { UniversiteComponent } from './layout/universite/universite.component';
+import { OffreComponent } from './layout/offre/offre.component';
 
 const appRoutes:Routes=[
   {path: 'header', component:HeaderComponent},
   {path: 'footer', component:FooterComponent},
   {path: 'navbar', component:NavbarComponent},
   {path: 'sidebar', component:SidebarComponent},
+  {path: 'ecole', component:EcoleComponent},
+  {path: 'universite', component:UniversiteComponent},
+  {path: 'offre', component:OffreComponent}
 
-
-]
+];
 
 @NgModule({
   declarations: [
@@ -25,11 +30,14 @@ const appRoutes:Routes=[
     HeaderComponent,
     FooterComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    EcoleComponent,
+    UniversiteComponent,
+    OffreComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, RouterModule.forRoot(appRoutes), AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/universite/universite.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UniversiteComponent } from './universite.component';
+
+describe('UniversiteComponent', () => {
+  let component: UniversiteComponent;
+  let fixture: ComponentFixture<UniversiteComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UniversiteComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UniversiteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
